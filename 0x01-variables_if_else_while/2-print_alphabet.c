e48fc412bb9864c88725f4c40b4fff6f64ec7267
phenzy <Adubazyfestus@yahoo.com>
2022-09-10 10:41:04 -0700

    tired

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - main block
+ * Description: print the alphabet in lowercase, followed by a new line
+ * Return: 0
+ */
+
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+
+	putchar('\n');
+	return (0);
+}
