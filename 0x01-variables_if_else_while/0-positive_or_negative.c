9a0d54160409d5b5b41235e57e3e6717d7ddd7a2
phenzy <Adubazyfestus@yahoo.com>
2022-09-10 07:02:36 -0700

    low level

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -18,15 +18,15 @@ int main(void);
 
 	if (n > 0)
 	{
-	printf("%d is positive\n", n);
+		printf("%i is positive\n", n);
 	}
 	else if (n == 0)
 	{
-	printf("%d is equal to 0\n", n);
+		printf("%i is equal to 0\n", n);
 	}
 	else
 	{
-	printf("%d is negative\n", n);
+		printf("%i is negative\n", n);
 	}
 
 	return (0);
