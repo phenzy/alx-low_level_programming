cfcd2199bd910cf54a018e219259f5893b7ec43c
phenzy <Adubazyfestus@yahoo.com>
2022-09-13 09:29:13 -0700

    writing _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include "<main.h>"
+/**
+ *  _putchar - writes the character c to stdout
+ * @char: The character to print
+ * Return: On success, char
+ * 0n error, 1 is returned
+ */
+
+int _putchar(char)
+{
+	_putchar("_");
+	_putchar("p");
+	_putchar("u");
+	_putchar("t");
+	_putchar("c");
+	_putchar("h");
+	_putchar("a");
+	_putchar("r");
+
+	return (0);
+
+}
