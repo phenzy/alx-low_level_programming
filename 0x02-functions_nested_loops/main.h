8b26aca2e9b34aa7593baf79000f6f873e649f8e
phenzy <Adubazyfestus@yahoo.com>
2022-09-14 06:01:18 -0700

    times table

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,7 +1,9 @@
-#ifndef MAIN_H_INCLUDED
-#define MAIN_H_INCLUDED
+#ifndef
+MAIN_H_INCLUDED
+#define
+MAIN_H_INCLUDED
 
-int _main(void)
+int _putchar(char);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
