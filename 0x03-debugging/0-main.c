27b8c4b7082bfeaeb5aa9b6cf6200da9193deff4
phenzy <Adubazyfestus@yahoo.com>
2022-09-15 03:03:21 -0700

    first debugging task

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include "main.h"
 /**
 * main - tests function that prints if integer is positive or negative
 * Return: 0
@@ -7,14 +7,9 @@
 int main(void)
 {
 	int i;
+
 	i = 98;
-	if (i > 100)
-	{
-		printf("positive");
-	}
-	else
-	{
-		printf("negative");
-	}
+	positive_or_negative(i);
+
 	return (0);
 }
