9752d705659c3fcf66692f60ec9750f5e58b6ac4
phenzy <Adubazyfestus@yahoo.com>
2022-09-14 17:41:02 -0700

    debugging

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+* main - tests function that prints if integer is positive or negative
+* Return: 0
+*/
+
+int main(void)
+{
+	int i;
+	i = 98;
+	if (i > 100)
+	{
+		printf("positive");
+	}
+	else
+	{
+		printf("negative");
+	}
+	return (0);
+}
