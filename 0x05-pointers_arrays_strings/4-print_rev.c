bed11d3f40a717b22616c29ff3edc81991459cfd
phenzy <Adubazyfestus@yahoo.com>
2022-09-19 12:09:24 -0700

    reverse string

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * print_rev - func that prints a string, in reverse, followed by a new line
  * @s: declaration of s and paramters for the function
--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,17 +1,23 @@
-#include "main.h"
 #include <stdio.h>
-
 /**
- * main - check code.
- *
+ * rev_string - function that reverses a string.
+ * @s: declaration of *s and paramters for the function
  * Return: Always 0.
  */
-int main(void)
+void rev_string(char *s)
 {
-	char s[10] = "My Schooln";
+	int f, r_s;
+	char character;
 
-	printf("%s\n", s);
-	rev_string(s);
-	printf("%s\n", s);
-	return (0);
+	for (f = '\0'; s[f] != 0; f++)
+	{
+	}
+	r_s = 0;
+	for (f = f - 1; r_s < f; r_s++)
+	{
+		character = s[f];
+		s[f] = s[r_s];
+		s[r_s] = character;
+		f--;
+	}
 }
