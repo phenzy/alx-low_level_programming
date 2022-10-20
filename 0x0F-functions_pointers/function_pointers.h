f148a11729c7823c21a111b41cf935b8b910258a
phenzy <Adubazyfestus@yahoo.com>
2022-10-20 07:35:54 -0700

    This is my lists file

--- 0x0F-functions_pointers/function_pointers.h
+++
@@ -1,9 +0,0 @@
-#ifndef MAIN_H_FILE
-#define MAIN__H_FILE
-#include <stdlib.h>
-
-void print_name(char *name, void (*f)(char *));
-void array_iterator(int *array, size_t size, void (*action)(int));
-int int_index(int *array, int size, int (*cmp)(int));
-
-#endif
